b51298d3eedfd67c2dd92d0bffff444144e6663b
Antonis Berkakis <aberkakis@gmail.com>
2018-01-15 23:58:53 +0000

    Add some logging

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,4 +1,5 @@
 defmodule Issues.GithubIssues do
+  require Logger
   @user_agent [{"User-agent", "Antonis Elixir Client"}]
   @github_url Application.get_env(:issues, :github_url)
 
@@ -9,15 +10,17 @@ defmodule Issues.GithubIssues do
   end
 
   def issues_url(user, project) do
-    IO.puts "#{@github_url}/repos/#{user}/#{project}/issues"
+    Logger.info "#{@github_url}/repos/#{user}/#{project}/issues"
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
   def handle_response({:ok, %{status_code: 200, body: body}}) do
+    Logger.debug fn -> inspect(body) end
     { :ok, Poison.Parser.parse!(body) }
   end
 
-  def handle_response({_, %{status_code: _, body: body}}) do
+  def handle_response({_, %{status_code: status, body: body}}) do
+    Logger.error "Error status #{status} returned"
     { :error, Poison.Parser.parse!(body) }
   end
 end
